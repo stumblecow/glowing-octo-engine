2e8c7dedb330e8e25a93add5e923a7c690385b3a
stumblecow <josephstanton@gmail.com>
2025-09-03 12:30:37 -0400

    Update gw_analysis.py

--- gw_analysis.py
+++ gw_analysis.py
@@ -149,7 +149,7 @@ def main():
     #data validation for editor
     for index, row_data in pivot_2027.iterrows():
       row_sum = row_data.drop('Chapter Delegates').sum()
-      if row_sum != pivot_2027['Chapter Delegates']:
+      if row_sum != row_data['Chapter Delegates']:
         print(f"Validation failed for {row['Chapter Delegates']}, check your math")
 
 
